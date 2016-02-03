9c62c9ef880885c61ef87c473103cb84f7f18608
Chris Simpkins <git.simpkins@gmail.com>
2016-02-03 14:18:48 -0500

    new positional argument parsing tests

--- tests/test_commandobject.py
+++ tests/test_commandobject.py
@@ -34,18 +34,56 @@ def test_sysargv_set():
 # ////////////////////////////////////////////////////////////
 
 def test_commandobj_property_argv():
-    """Test: test obj.argv Command object property uses 0 based index for first positional argument, not executable"""
+    """Test: obj.argv Command object property uses 0 based index for first positional argument, not executable"""
     set_sys_argv()
     c = Command()
     assert c.argv == ['subcmd', '-s', '--long', '-n', 'shortdef', '--name', 'longdef', '--nameeq=longdefeq', 'lastpos']
 
 
 def test_commandobj_property_argc():
-    """Test: test that obj.argc is defined with appropriate argument length"""
+    """Test: obj.argc is defined with appropriate argument length"""
     set_sys_argv()
     c = Command()
     assert c.argc == 9
 
 
+def test_commandobj_property_arg0():
+    """Test: obj.arg0 is defined as the first positional argument"""
+    set_sys_argv()
+    c = Command()
+    assert c.arg0 == "subcmd"
 
 
+def test_commandobj_property_arg1():
+    """Test: obj.arg1 is defined as second positional argument"""
+    set_sys_argv()
+    c = Command()
+    assert c.arg1 == "-s"
+
+
+def test_commandobj_property_arg2():
+    """Test: obj.arg2 is defined as the third positional argument"""
+    set_sys_argv()
+    c = Command()
+    assert c.arg2 == "--long"
+
+
+def test_commandobj_property_arg3():
+    """Test: obj.arg3 is defined as the fourth positional argument"""
+    set_sys_argv()
+    c = Command()
+    assert c.arg3 == "-n"
+
+
+def test_commandobj_property_arg4():
+    """Test: obj.arg4 is defined as the fifth positional argument"""
+    set_sys_argv()
+    c = Command()
+    assert c.arg4 == "shortdef"
+
+
+def test_commandobj_property_arglp():
+    """Test: obj.arglp is defined as the last positional argument"""
+    set_sys_argv()
+    c = Command()
+    assert c.arglp == "lastpos"
