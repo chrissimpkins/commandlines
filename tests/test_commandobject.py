131f4df0ac2940a206ca9d72e45b6492a5194916
Chris Simpkins <git.simpkins@gmail.com>
2016-02-03 14:07:01 -0500

    new command object argc property test

--- tests/test_commandobject.py
+++ tests/test_commandobject.py
@@ -14,7 +14,7 @@ def set_sys_argv():
     sys.argv = test_command_1.split(" ")
 
 
-# ////////////////////////////////////////////
+# ///////////////////////////////////////////
 #
 # TESTS : Mock sys.argv Setup
 #
@@ -24,7 +24,7 @@ def test_sysargv_set():
     """Test: confirm that sys.argv is mocked appropriately with the test command"""
     # set the sys.argv
     set_sys_argv()
-    assert sys.argv == ['executable', 'subcmd', '-s', '--long', '-n', 'shortdef', '--name', 'longdef', '--nameq=longdef', 'lastpos']
+    assert sys.argv == ['executable', 'subcmd', '-s', '--long', '-n', 'shortdef', '--name', 'longdef', '--nameeq=longdef', 'lastpos']
 
 
 # ////////////////////////////////////////////
@@ -37,6 +37,14 @@ def test_commandobj_property_argv():
     """Test: test obj.argv Command object property uses 0 based index for first positional argument, not executable"""
     set_sys_argv()
     c = Command()
-    assert c.argv == ['subcmd', '-s', '--long', '-n', 'shortdef', '--name', 'longdef', '--nameq=longdef', 'lastpos']
+    assert c.argv == ['subcmd', '-s', '--long', '-n', 'shortdef', '--name', 'longdef', '--nameeq=longdef', 'lastpos']
+
+
+def test_commandobj_property_argc():
+    """Test: test that obj.argc is defined with appropriate argument length"""
+    set_sys_argv()
+    c = Command()
+    assert c.argc == 9
+
 
 
