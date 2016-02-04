baab1b900429214fc9f74ddc3558c4875ce57734
Chris Simpkins <git.simpkins@gmail.com>
2016-02-04 17:26:54 -0500

    new tests, updated tests

--- tests/test_commandobject2.py
+++ tests/test_commandobject2.py
@@ -7,8 +7,7 @@ import pytest
 
 from commandlines import Command
 
-test_command_1 = "executable subcmd -s --long -n shortdef --name longdef --nameeq=longdefeq lastpos"
-test_command_2 = "executable -mops -t lastpos"
+test_command_1 = "executable -s lastpos"
 
 
 # ///////////////////////////////////////////
@@ -21,10 +20,6 @@ def set_sys_argv():
     sys.argv = test_command_1.split(" ")
 
 
-def set_sys_argv2():
-    sys.argv = test_command_2.split(" ")
-
-
 # ///////////////////////////////////////////
 #
 # TESTS : Mock sys.argv Setup
@@ -35,7 +30,7 @@ def test_sysargv_set():
     """Test: confirm that sys.argv is mocked appropriately with the test command"""
     # set the sys.argv
     set_sys_argv()
-    assert sys.argv == ['executable', 'subcmd', '-s', '--long', '-n', 'shortdef', '--name', 'longdef', '--nameeq=longdefeq', 'lastpos']
+    assert sys.argv == ['executable', '-s', 'lastpos']
 
 
 # ////////////////////////////////////////////////////////////
@@ -48,49 +43,49 @@ def test_commandobj_property_argv():
     """Test: obj.argv Command object property uses 0 based index for first positional argument, not executable"""
     set_sys_argv()
     c = Command()
-    assert c.argv == ['subcmd', '-s', '--long', '-n', 'shortdef', '--name', 'longdef', '--nameeq=longdefeq', 'lastpos']
+    assert c.argv == ['-s', 'lastpos']
 
 
 def test_commandobj_property_argc():
     """Test: obj.argc is defined with appropriate argument length"""
     set_sys_argv()
     c = Command()
-    assert c.argc == 9
+    assert c.argc == 2
 
 
 def test_commandobj_property_arg0():
     """Test: obj.arg0 is defined as the first positional argument"""
     set_sys_argv()
     c = Command()
-    assert c.arg0 == "subcmd"
+    assert c.arg0 == "-s"
 
 
 def test_commandobj_property_arg1():
     """Test: obj.arg1 is defined as second positional argument"""
     set_sys_argv()
     c = Command()
-    assert c.arg1 == "-s"
+    assert c.arg1 == "lastpos"
 
 
 def test_commandobj_property_arg2():
     """Test: obj.arg2 is defined as the third positional argument"""
     set_sys_argv()
     c = Command()
-    assert c.arg2 == "--long"
+    assert c.arg2 == ""
 
 
 def test_commandobj_property_arg3():
     """Test: obj.arg3 is defined as the fourth positional argument"""
     set_sys_argv()
     c = Command()
-    assert c.arg3 == "-n"
+    assert c.arg3 == ""
 
 
 def test_commandobj_property_arg4():
     """Test: obj.arg4 is defined as the fifth positional argument"""
     set_sys_argv()
     c = Command()
-    assert c.arg4 == "shortdef"
+    assert c.arg4 == ""
 
 
 def test_commandobj_property_arglp():
@@ -104,14 +99,14 @@ def test_commandobj_property_subcmd():
     """Test: obj.subcmd is defined as the first positional argument"""
     set_sys_argv()
     c = Command()
-    assert c.subcmd == "subcmd"
+    assert c.subcmd == "-s"
 
 
 def test_commandobj_property_subsubcmd():
     """Test: obj.subsubcmd is defined as the second positional argument"""
     set_sys_argv()
     c = Command()
-    assert c.subsubcmd == "-s"
+    assert c.subsubcmd == "lastpos"
 
 
 def test_commandobj_property_has_args():
@@ -146,9 +141,8 @@ def test_commandobj_property_switches():
     set_sys_argv()
     c = Command()
     assert isinstance(c.switches, set)
-    assert len(c.switches) == 4
-    for x in ['s', 'long', 'n', 'name']:
-        assert x in c.switches
+    assert 's' in c.switches
+    assert len(c.switches) == 1
 
 
 # ////////////////////////////////////////////////////////////
@@ -159,13 +153,10 @@ def test_commandobj_property_switches():
 
 def test_commandobj_property_mops():
     """Test: obj.mops property is defined with instantiated Mops object and includes correct characters"""
-    set_sys_argv2()
+    set_sys_argv()
     c = Command()
     assert isinstance(c.mops, set)
-    assert len(c.mops) == 4
-    for x in "mops":
-        assert x in c.mops
-    assert "t" not in c.mops  # confirm that 't' character from short switch is not included
+    assert len(c.mops) == 0 # should be empty, no mops included in command
 
 
 # ////////////////////////////////////////////////////////////
@@ -179,9 +170,4 @@ def test_commandobj_property_definitions():
     set_sys_argv()
     c = Command()
     assert isinstance(c.defs, dict)
-    assert len(c.defs) == 3
-    expected_keys = ['n', 'name', 'nameeq']
-    observed_keys = c.defs.keys()
-    assert len(observed_keys) == len(expected_keys)
-    for x in observed_keys:
-        assert x in expected_keys
+    assert len(c.defs) == 0
