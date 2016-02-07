5c44138e1a546f53810df2a4244b455425adb299
Chris Simpkins <git.simpkins@gmail.com>
2016-02-07 09:02:29 -0500

    new Mops class tests

---
+++ tests/test_mops.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import shlex
+import pytest
+
+from commandlines.library import Mops
+
+# TESTS OVERVIEW: Command object getter method tests
+
+test_command_1 = "executable subcmd -s --long -n shortdef --name longdef --nameeq=longdefeq lastpos"
+test_command_2 = "executable -s lastpos"
+test_command_3 = "executable --long lastpos"
+test_command_4 = "executable -v -V lastpos"
+test_command_5 = "executable --test-option lastpos"
+test_command_6 = "apt-get install git-all"
+test_command_7 = "git commit -m 'initial commit'"
+test_command_8 = "find . -name tests/aaa.txt"
+test_command_9 = "executable -mops -t lastpos"
+test_command_10 = "executable subcmd subsubcmd"
+test_command_11 = "executable -mops -test lastpos"
+test_command_empty_1 = "executable"
+test_command_empty_2 = "exe-dash"
+
+
+def create_argv(argstring):
+    return shlex.split(argstring)[1:]
+
+# BEGIN TESTS
+
+
+def test_mops_instantiation():
+    mops_set = Mops(create_argv(test_command_9))
+    assert len(mops_set) == 4
+    assert ("m" in mops_set) == True
+    assert ("o" in mops_set) == True
+    assert ("p" in mops_set) == True
+    assert ("s" in mops_set) == True
+    assert ("z" in mops_set) == False
+
+
+def test_mops_instantiation_multiple_mops_in_command():
+    mops_set = Mops(create_argv(test_command_11))
+    assert len(mops_set) == 6    # set data type should eliminate multiple requested switches that are the same
+    assert ("m" in mops_set) == True
+    assert ("o" in mops_set) == True
+    assert ("p" in mops_set) == True
+    assert ("s" in mops_set) == True
+    assert ("t" in mops_set) == True
+    assert ("e" in mops_set) == True
+    assert ("z" in mops_set) == False
+
+
+def test_mops_contains():
+    mops_set = Mops(create_argv(test_command_9))
+    assert len(mops_set) == 4
+    assert mops_set.contains(("m")) == True
+    assert mops_set.contains(("m", "o")) == True
+    assert mops_set.contains(("m", "o", "p")) == True
+    assert mops_set.contains(("m", "o", "p", "s")) == True
+    assert mops_set.contains(("b")) == False       # switch character that is not present should yield False
+    assert mops_set.contains(("m", "b")) == False  # single missing switch character should yield False
